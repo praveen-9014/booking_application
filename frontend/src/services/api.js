eea6ec88b90a68803431f38bceacbfd02370b98a
praveen-9014 <praveen901471@gmail.com>
2025-12-19 12:09:14 +0530

    Fix Vercel frontend deployment config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://booking-application-g1ov.onrender.com/api';
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
